6ad648a805437874e569e5e2d533f62a89a70e87
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-02-19 12:04:33 +0100

    implemented react router and basic header and footer layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
-
+import { Routes, Route } from "react-router";
+import Header from "./components/partials/Header";
+import Footer from "./components/partials/Footer";
+import Home from "./components/home/Home";
 function App() {
-	return <h1>Hello World</h1>;
+	return (
+		<div className="app-container pt-5">
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
