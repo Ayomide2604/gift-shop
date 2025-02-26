ceef7ec46982999cb43cea282f7d6041b57c25e3
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-02-26 05:35:18 +0100

    updated product store to use pagination

--- src/components/partials/Pagination.jsx
+++ src/components/partials/Pagination.jsx
@@ -1,40 +1,63 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
+import useProductStore from "../../../store/productStore";
+
 const Pagination = () => {
+	const { totalProducts, currentPage, fetchProducts, nextPage, previousPage } =
+		useProductStore();
+
+	const pageSize = 2;
+	const totalPages = Math.ceil(totalProducts / pageSize);
+
+	const handlePageChange = (page) => {
+		if (page > 0 && page <= totalPages) {
+			fetchProducts(page);
+		}
+	};
+
 	return (
 		<Row>
 			<Col className="col-12 d-flex justify-content-center">
-				<div>
-					<nav aria-label="Page navigation example">
-						<ul className="pagination">
-							<li className="page-item">
-								<a className="page-link" href="#">
-									Previous
-								</a>
-							</li>
-							<li className="page-item">
-								<a className="page-link" href="#">
-									1
-								</a>
-							</li>
-							<li className="page-item">
-								<a className="page-link" href="#">
-									2
-								</a>
-							</li>
-							<li className="page-item">
-								<a className="page-link" href="#">
-									3
-								</a>
-							</li>
-							<li className="page-item">
-								<a className="page-link" href="#">
-									Next
-								</a>
-							</li>
-						</ul>
-					</nav>
-				</div>
+				<nav aria-label="Page navigation">
+					<ul className="pagination">
+						<li className={`page-item ${!previousPage ? "disabled" : ""}`}>
+							<button
+								className="page-link"
+								onClick={() => handlePageChange(currentPage - 1)}
+							>
+								Previous
+							</button>
+						</li>
+
+						{[...Array(totalPages)].map((_, index) => {
+							const page = index + 1;
+							return (
+								<li
+									key={page}
+									className={`page-item ${
+										currentPage === page ? "active" : ""
+									}`}
+								>
+									<button
+										className="page-link"
+										onClick={() => handlePageChange(page)}
+									>
+										{page}
+									</button>
+								</li>
+							);
+						})}
+
+						<li className={`page-item ${!nextPage ? "disabled" : ""}`}>
+							<button
+								className="page-link"
+								onClick={() => handlePageChange(currentPage + 1)}
+							>
+								Next
+							</button>
+						</li>
+					</ul>
+				</nav>
 			</Col>
 		</Row>
 	);
--- store/productStore.js
+++ store/productStore.js
@@ -5,17 +5,26 @@ const VITE_API_URL = import.meta.env.VITE_API_URL;
 
 const useProductStore = create((set) => ({
 	products: [],
+	totalProducts: 0,
+	nextPage: null,
+	previousPage: null,
 	product: null,
 	Loading: false,
 	error: null,
 
-	fetchProducts: async () => {
+	fetchProducts: async (page = 1) => {
 		set((state) => ({ ...state, Loading: true, error: null }));
 		try {
-			const response = await axios.get(`${VITE_API_URL}/products/`);
+			const response = await axios.get(
+				`${VITE_API_URL}/products/?page=${page}`
+			);
 			set((state) => ({
 				...state,
 				products: response.data.results,
+				totalProducts: response.data.count,
+				nextPage: response.data.next,
+				previousPage: response.data.previous,
+				currentPage: page,
 				Loading: false,
 			}));
 		} catch (error) {
