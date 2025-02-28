bd6152f83b09a344bc7b685f4607114418038e14
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-02-28 09:37:29 +0100

    Fix import path capitalization for auth CSS file

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Form, Button, Container, Card } from "react-bootstrap";
-import "./Auth.css";
+import "./auth.css";
 import useAuthStore from "../../../store/authStore";
 
 const Signup = () => {
