ca679bc94dfacdc14d7ac7dd9ec06aaa70b6e943
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-02-28 16:04:05 +0100

    Improve Header component with responsive navbar and mobile-friendly navigation

--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -1,73 +1,89 @@
-import React, { useEffect } from "react";
-import { Container } from "react-bootstrap";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import React, { useEffect, useState } from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { BsCart4 } from "react-icons/bs";
 import useAuthStore from "./../../../store/authStore";
 import useCartStore from "./../../../store/cartStore";
-
 import "./partials.css";
 
 const Header = () => {
 	const { cart, fetchCart } = useCartStore();
 	const user = useAuthStore((state) => state.user);
 	const logout = useAuthStore((state) => state.logout);
+	const [expanded, setExpanded] = useState(false);
 
 	useEffect(() => {
 		fetchCart();
-	}, []);
+	}, [fetchCart]);
+
+	const handleNavLinkClick = () => {
+		setExpanded(false);
+	};
 
 	return (
 		<Navbar
 			fixed="top"
 			bg="dark"
-			collapseOnSelect
 			expand="lg"
 			className="bg-body-tertiary"
+			expanded={expanded}
 		>
 			<Container fluid className="header-container px-5">
-				<Navbar.Brand as={Link} to="/">
+				<Navbar.Brand as={Link} to="/" onClick={handleNavLinkClick}>
 					Gift Shop
 				</Navbar.Brand>
-				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Toggle
+					aria-controls="responsive-navbar-nav"
+					onClick={() => setExpanded(expanded ? false : "expanded")}
+				/>
 				<Navbar.Collapse id="responsive-navbar-nav">
 					<Nav className="me-auto">
-						<Nav.Link as={Link} to="/about">
+						<Nav.Link as={Link} to="/about" onClick={handleNavLinkClick}>
 							About
 						</Nav.Link>
-						<Nav.Link as={Link} to="/categories">
+						<Nav.Link as={Link} to="/categories" onClick={handleNavLinkClick}>
 							Categories
 						</Nav.Link>
-						<Nav.Link as={Link} to="/packages">
+						<Nav.Link as={Link} to="/packages" onClick={handleNavLinkClick}>
 							Packages
 						</Nav.Link>
-						<Nav.Link as={Link} to="/products">
+						<Nav.Link as={Link} to="/products" onClick={handleNavLinkClick}>
 							Products
 						</Nav.Link>
-						<Nav.Link as={Link} to="/contact">
+						<Nav.Link as={Link} to="/contact" onClick={handleNavLinkClick}>
 							Contact
 						</Nav.Link>
 					</Nav>
 					<Nav>
 						{user ? (
 							<>
-								<Nav.Link as={Link} to="/dashboard">
+								<Nav.Link
+									as={Link}
+									to="/dashboard"
+									onClick={handleNavLinkClick}
+								>
 									(Welcome, {user.username || user.email})
 								</Nav.Link>
-								<Nav.Link onClick={logout}>Logout</Nav.Link>
+								<Nav.Link
+									onClick={() => {
+										logout();
+										handleNavLinkClick();
+									}}
+								>
+									Logout
+								</Nav.Link>
 							</>
 						) : (
 							<>
-								<Nav.Link as={Link} to="/login">
+								<Nav.Link as={Link} to="/login" onClick={handleNavLinkClick}>
 									Login
 								</Nav.Link>
-								<Nav.Link as={Link} to="/signup">
+								<Nav.Link as={Link} to="/signup" onClick={handleNavLinkClick}>
 									Signup
 								</Nav.Link>
 							</>
 						)}
-						<Nav.Link as={Link} to="/cart">
+						<Nav.Link as={Link} to="/cart" onClick={handleNavLinkClick}>
 							<BsCart4
 								className="cart-icon position-absolute"
 								style={{ fontSize: "20px" }}
