ddf7ebbd6cec2374cad28a00f5de507228408c85
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-03-01 07:30:22 +0100

    Add Profile page and update Dashboard navigation

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -6,12 +6,14 @@ import {
 	RiSettingsLine,
 	RiMapPinLine,
 	RiLogoutCircleLine,
+	RiAccountCircleLine,
 } from "react-icons/ri";
 import AddressList from "../shipping/AddressList";
 import OrderList from "../orders/OrderList";
 import OrderDetail from "../orders/OrderDetail";
 import useAuthStore from "./../../../store/authStore";
 import "./dashboard.css";
+import Profile from "./Profile";
 
 const Dashboard = () => {
 	const user = useAuthStore((state) => state.user);
@@ -31,9 +33,7 @@ const Dashboard = () => {
 					<div className="user-info rounded-4 border bg-white mb-4">
 						<div className="text-center p-5 border-bottom">
 							<img
-								src={
-									user?.profile?.profile_picture_url || "/avatar.png"
-								}
+								src={user?.profile?.profile_picture_url || "/avatar.png"}
 								alt="customer"
 								className="img-fluid rounded-circle mb-4"
 								style={{ width: "150px", height: "150px", objectFit: "cover" }}
@@ -45,9 +45,12 @@ const Dashboard = () => {
 						</div>
 						<div className="p-4">
 							<Link className="dropdown-item active" to="/dashboard">
+								<RiAccountCircleLine /> Profile
+							</Link>
+							<Link className="dropdown-item active" to="/dashboard/orders">
 								<RiFileListLine /> Orders
 							</Link>
-							<Link className="dropdown-item" to="account-settings.html">
+							<Link className="dropdown-item" to="#">
 								<RiSettingsLine /> Settings
 							</Link>
 							<Link className="dropdown-item" to="/dashboard/address">
@@ -61,7 +64,8 @@ const Dashboard = () => {
 				</Col>
 				<Col lg={8} className="content-area">
 					<Routes>
-						<Route index element={<OrderList />} />
+						<Route index element={<Profile />} />
+						<Route path="/orders" element={<OrderList />} />
 						<Route path="/orders/:orderId" element={<OrderDetail />} />
 						<Route path="/address" element={<AddressList />} />
 					</Routes>
